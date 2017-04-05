c87283cb8f03ad65be720435205c282dfcb9a740
rush1506 <vuthanhtam1506@gmail.com>
2017-04-05 15:25:57 +0700

    added updateDictionary, UpdateDictionaryValues, createDictionaryValues function

--- search/api/BuildIndex.py
+++ search/api/BuildIndex.py
@@ -2,6 +2,9 @@ from codecs import open
 from Normalize import Normalize
 from time import localtime, strftime
 import datrie
+from collections import defaultdict
+
+WordIndex = defaultdict(list)
 
 def BuildIndex(DataPath, OutputPath):
 	print("Enter %s" %DataPath)
@@ -14,13 +17,13 @@ def BuildIndex(DataPath, OutputPath):
 			print("Process %s in line %s" %(line, LinePosition))
 			print("Timestamp: " + strftime("%a, %d %b %Y %H:%M:%S", localtime()))
 			LinePosition = LinePosition + 1
-			updateNewIndexFrom(line)
+			updateNewIndexFrom(line, LinePosition)
 	print("Export index to %s" %OutputPath)
 	print("Timestamp: " + strftime("%a, %d %b %Y %H:%M:%S", localtime()))
 	exportIndex(OutputPath)
 				
 
-def updateNewIndexFrom(CurrentLine):
+def updateNewIndexFrom(CurrentLine, LinePosition):
 	print("Split into single word at " + strftime("%a, %d %b %Y %H:%M:%S", localtime()))
 	RawWordsList = CurrentLine.split()
 	WordPosition = 0
@@ -30,6 +33,42 @@ def updateNewIndexFrom(CurrentLine):
 		nWord = Normalize(RawWord)
 		print("Add %s to words' dictionary" %(nWord, WordPosition))
 		print("Timestamp: " + strftime("%a, %d %b %Y %H:%M:%S", localtime()))
-		updateDictionary(nWord)
+		updateDictionary(nWord, LinePosition, WordPosition)
+		WordPosition = WordPosition + 1
+	return true
+	
+def updateDictionary(word, LinePosition, WordPosition):
+	global WordIndex
+	if word in WordIndex:
+		updateDictionaryValues(word, LinePosition, WordPosition)
+	else:
+		createDictionaryValues(word, LinePosition, WordPosition)
+		
+			
+def createDictionaryValues(word, LinePosition, WordPosition):
+	print("%s is a new word, add new word to dictionary" %word)
+	print("Timestamp: " + strftime("%a, %d %b %Y %H:%M:%S", localtime()))
+	NewPositionEntry = (LinePosition, WordPosition)
+	print("Create new entry for: ")
+	print(NewPositionEntry)
+	print("Timestamp: " + strftime("%a, %d %b %Y %H:%M:%S", localtime()))
+	NewIndexEntry = [(word, 1), (word, NewPositionEntry)]
+	for word, NewEntry in NewIndexEntry: 
+		wordIndex[word].append(NewEntry)
+
+def updateDictionaryValues(word, LinePosition, WordPosition):
+	print("%s is already an index member, update %s value" %(word, word))
+	print("Timestamp: " + strftime("%a, %d %b %Y %H:%M:%S", localtime()))
+	NewPositionEntry = (LinePosition, WordPosition)
+	WordCount = WordIndex[word][0]
+	print("Old word count: %s" %WordCount)
+	WordCount = WordCount + 1
+	WordIndex[word][0] = WordCount
+	print("Update new word count: %s" %WordCount)
+	print("Timestamp: " + strftime("%a, %d %b %Y %H:%M:%S", localtime()))
+	WordIndex[word].append(NewPositionEntry)
+	print("Append to word index: ")
+	print(NewPositionEntry)
+	print("Timestamp: " + strftime("%a, %d %b %Y %H:%M:%S", localtime()))
 	return true
 			
--- search/api/test.py
+++ search/api/test.py
@@ -1,3 +1,13 @@
-a= 'a'
-b= 'b'
-print("%s %s"%(a, b))
+from collections import defaultdict
+
+WordIndex = defaultdict(list)
+
+word = "test"
+NewPositionEntry = ('line', 'pos')
+NewIndexEntry = [(word, 1), (word, NewPositionEntry)]
+for word, NewEntry in NewIndexEntry:
+	WordIndex[word].append(NewEntry)
+	
+WordIndex[word].append(NewPositionEntry)
+	
+print(WordIndex[word])
