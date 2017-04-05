876b7df246477d4bec7c10cdfa2a57d0be2dc78b
rush1506 <vuthanhtam1506@gmail.com>
2017-04-05 16:09:12 +0700

    added putToLowerCase function in normalizing work

--- search/api/Normalize.py
+++ search/api/Normalize.py
@@ -1,4 +1,8 @@
 def NormalizeWord(word):
 	for Character in ['[', ']', '(', ')']:
 		word = word.replace(Character, "")
+	word = putToLowCase(word)
 	return word
+	
+def putToLowCase(word):
+	return word.lower()
